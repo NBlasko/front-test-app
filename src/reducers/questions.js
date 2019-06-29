9f3aaee8bbd9c79a3779986ee4f414a9ef472d0e
NBlasko <epodos.blasko@gmail.com>
2019-06-30 00:29:27 +0200

    setup store and first reducer

---
+++ src/reducers/questions.js
@@ -0,0 +1,16 @@
+
+
+const initialstate = [];
+
+
+
+const questions = (state = initialstate, action) => {
+    switch (action.type) {
+
+
+        default:
+            return state
+    }
+}
+
+export default questions;
