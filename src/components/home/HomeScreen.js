be8f6d1119d78ac4d4ad5d14cb932496b5ec8e4f
NBlasko <epodos.blasko@gmail.com>
2019-06-30 01:20:39 +0200

    finished HomeScreen logic

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -5,11 +5,16 @@ class HomeScreen extends Component {
 
 
   render() {
-    
+    const quiz1 = localStorage.getItem('quiz1') || 0;
+    const quiz2 = localStorage.getItem('quiz2') || 0;
+    const quiz3 = localStorage.getItem('quiz3') || 0;
+
     return (
       <div>
         Home
-
+        <div>  1. {quiz1}  </div>
+        <div>  2. {quiz2}  </div>
+        <div>  3. {quiz3}  </div>
       </div>
     );
   }
