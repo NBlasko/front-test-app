55886a6a3eddafa3684020c93b6deea734c024fa
NBlasko <epodos.blasko@gmail.com>
2019-06-30 01:01:43 +0200

    output single question with click logic

--- src/components/main/MainScreen.js
+++ src/components/main/MainScreen.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchQuestions } from '../../actions'
-
+import SingleQuestion from './SingleQuestion'
 
 
 class MainScreen extends Component {
 
+  state = {
+    questionNumber: 0,
+    usersAnswer: "",
+  }
 
   componentDidMount() {
     this.props.fetchQuestions();
   }
 
+  handleAnswer = (e) => {
+
+    // prevent answering more than once
+    if (this.state.usersAnswer) return;
+    const usersAnswer = e.target.value;
+    this.setState({ usersAnswer })
+
+    if (e.target.value === this.props.questions[this.state.questionNumber].continent) {
+      console.log("bravo");
+    }
+  }
+
+  
   render() {
+    const { questionNumber, usersAnswer } = this.state;
+    const { questions } = this.props;
     return (
       <div>
-       Main
+        <div>
+          Question {questionNumber + 1} out of 5
+          </div>
+        {(questions.length !== 5)
+          ? null
+          : <SingleQuestion
+            question={questions[questionNumber]}
+            handleAnswer={this.handleAnswer}
+            usersAnswer={usersAnswer}
+          />
+        }
+
+      
+
       </div>
     );
   }
 }
-
 
 const mapStateToProps = ({ questions }) => {
   return { questions }
---
+++ src/components/main/SingleQuestion.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const SingleQuestion = (props) => {
+    console.log("props", props);
+    const { image, answers, continent } = props.question;
+    const { handleAnswer, usersAnswer } = props;
+    const isRight = (usersAnswer === continent);
+    const buttons =
+        [0, 1, 2].map(
+            num =>
+                <div key={num}>
+                    <button value={answers[num]} onClick={handleAnswer}  >
+                        {answers[num]}
+                        <span> {(usersAnswer && answers[num] === continent)
+                            ? "bravo"
+                            : null
+                        }
+
+                            {(!isRight && answers[num] === usersAnswer)
+                                ? "cvrc"
+                                : null
+                            } </span>
+                    </button>
+                </div>
+        )
+    return (
+        <div>
+            <img src={image} alt="continent" />
+            {buttons}
+        </div>
+    )
+}
+export default SingleQuestion;
