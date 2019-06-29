654578c0ea7fcaa5d0a35059b176465aaf78df97
NBlasko <epodos.blasko@gmail.com>
2019-06-30 00:50:18 +0200

    populate navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import { NavLink, withRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
+  const { pathname } = props.location;
+  let naviLinks = null;
+  if (pathname !== "/main")
+    naviLinks = <ul>
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink to='/main'>Main</NavLink></li>
+    </ul>;
+
+  if (pathname === "/results")
+    naviLinks = <NavLink exact to="/">Finish</NavLink>;
 
   return (
     <div >
-      nav
+      {naviLinks}
     </div>
   )
 }
-export default Navbar
+export default withRouter(Navbar)
