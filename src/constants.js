5b7bf366aa4141d3a4f8873e07ac434fb8bb482d
NBlasko <epodos.blasko@gmail.com>
2019-06-30 00:38:51 +0200

    finished question reducer

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,18 @@
 // fetch all questions from API
 export const API_URL = 'https://api.myjson.com/bins/a6da9';
 
+//all continents
+
+export const CONTINENTS = [
+    "Europe",
+    "South America",
+    "Africa",
+    "Antarctica",
+    "Oceania",
+    "Asia",
+    "North America"
+];
+
 // redux types 
 export const FETCH_QUESTIONS = 'FETCH_QUESTIONS';
 
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,12 +1,48 @@
-
+import { FETCH_QUESTIONS, CONTINENTS } from '../constants'
 
 const initialstate = [];
 
 
+const shuffleQuestions = (array) => {
+    //Shuffle fetched array
+    const shuffled = array.sort(() => 0.5 - Math.random());
+
+    // Get sub-array of first 5 elements after shuffled
+    return shuffled.slice(0, 5);
+}
+
+const appendWrongAnswers = (array) => {
+    array.forEach(element => {
+       
+        const filteredContinents =
+
+        // remove correct answer
+         CONTINENTS.filter((continent) => continent !== element.continent)
+
+         //shuffle remaining answers
+         .sort(() => 0.5 - Math.random())
+
+         // hold only two of remaining answers
+         .slice(0, 2);
+
+         // append the righ answer with the wrong ones
+         filteredContinents.push(element.continent);
+
+         // shuffle so the right answer won't be the necessarily last in the array
+         filteredContinents.sort(() => 0.5 - Math.random());
+
+         element.answers = filteredContinents;
+         
+    });
+    return array;
+}
 
 const questions = (state = initialstate, action) => {
     switch (action.type) {
 
+        case FETCH_QUESTIONS:
+            const shuffledPayload = shuffleQuestions([...action.payload]);
+            return appendWrongAnswers(shuffledPayload);
 
         default:
             return state
